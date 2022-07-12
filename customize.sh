1e4bb6e299d89fb5b57a61dd3bb78ce68e7c98f7
kakolukia <kakolukia.gavin@gmail.com>
2022-07-12 22:37:35 +0800

    update

--- customize.sh
+++ customize.sh
@@ -11,5 +11,6 @@ sed -i 's/192.168.1.1/192.168.6.1/g' openwrt/package/base-files/files/bin/config
 #2. Clear the login password
 #sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/emortal/default-settings/files/99-default-settings
 
-#3. Replace with JerryKuKu’s Argon
+#3. Change the default theme
 #rm openwrt/package/lean/luci-theme-argon -rf
+sed -i 's/+luci-theme-bootstrap/+luci-theme-argon-dark-mod/g' openwrt/feeds/luci/collections/luci/Makefile
